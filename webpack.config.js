e8f787e04f06143d2b28ce6eeddc361c1af3dd3e
sehailey <sarah.elizabeth.hailey@gmail.com>
2018-10-18 12:59:00 -0400

    created components folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
 const path = require('path')
 
 module.exports = {
-    entry: [
-        '@babel/polyfill', // enables async-await
-        './index.js'
-    ],
+  entry: [
+    '@babel/polyfill', // enables async-await
+    './index.js'
+  ],
 
-    output: {
-        filename: 'bundle.js'
-    },
+  output: {
+    filename: 'bundle.js'
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                //use: ['babel-loader']
-                loader: 'babel-loader'
-            }
-        ]
-    },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
 
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    }
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
 }
