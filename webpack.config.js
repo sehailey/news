8d5014e14512298232f7d88209257ed9faa6134a
sehailey <sarah.elizabeth.hailey@gmail.com>
2018-10-18 12:25:54 -0400

    restructure to client

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+    entry: [
+        '@babel/polyfill', // enables async-await
+        './index.js'
+    ],
+
+    output: {
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                //use: ['babel-loader']
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    }
+}
